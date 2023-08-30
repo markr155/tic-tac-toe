3c7052b3ad659dd9010997139207e5d6a14550ba
markr155 <markricho155@gmail.com>
2023-08-30 19:05:15 +1000

    cells display on click and board array updates

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
 Tic Tac Toe
 Main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories. 
 
-<!-- 1. create gameboard array inside of gameboard object -->
 
-<!-- 2. create player factories -->
-
-<!-- 3. create gameController object -->
-
-<!-- 4. display board to screen -->
-
-
-
-player clicks on the board
-check if click is legal move
-display marker
-    refresh whole board or just each cell?
+To Do
     add image(x, o) that appears, some transform on entry
-
+    
 check if game finish
 change turn to opponent
--- script.js
+++ script.js
@@ -17,18 +17,31 @@ const gameBoard = (() => {
         //clear board
         while (gameContainer.childElementCount > 0){ gameContainer.removeChild(gameContainer.lastChild)};
         //displays board
-        board.forEach(row => {
+        board.forEach((row, rowNum) => {
             row.forEach((col, index) => {
                 const newCell = document.createElement('button');
+                newCell.dataset.row = rowNum;
+                newCell.dataset.col = index;
                 newCell.setAttribute('class', 'cell');
                 newCell.textContent = col.getValue();
-                // newCell.addEventListener('click', gameController.playRound);
+                newCell.addEventListener('click', gameController.playRound);
                 gameContainer.appendChild(newCell);
             })
         })
     };
 
-    return { getBoard, displayBoard };
+    const addMarker = (row, col, player) => {
+        board[row][col].addToken(player.getToken());
+        console.log(`row: ${row},
+        col: ${col},
+        token: ${player.getToken()}`);
+    };
+
+    return {
+        getBoard, 
+        displayBoard,
+        addMarker
+         };
 })();
 
 const Player = (token) => {
@@ -39,7 +52,7 @@ const Player = (token) => {
 };
 
 function Cell() {
-    let value = 0;
+    let value = '';
 
     const addToken = (player) => {
         value = player;
@@ -55,8 +68,8 @@ function Cell() {
 }
 
 const gameController = (() => {
-    const player1 = Player('x');
-    const player2 = Player('o');
+    const player1 = Player('X');
+    const player2 = Player('O');
     const players = [player1, player2];
 
     let activePlayer = players[0];
@@ -64,12 +77,33 @@ const gameController = (() => {
 
     
 
-    const playRound = () => {
-        console.log();
+    const playRound = (cellClicked) => {
+        const cell = cellClicked.target;
+        const row = cell.dataset.row;
+        const col = cell.dataset.col;
+        console.log(cell.target);
+        //check if cell is occupied - check board array
+            //if yes not legal, display error, end early no changes
+            //if no continue
+        if (gameBoard.getBoard()[row][col].getValue() !== ""){
+            console.log('Cell not valid');    
+            return;
+        };
+        //add player token to array
+        gameBoard.addMarker(row, col, getActivePlayer());
+        //update score
+        //update board
+        gameBoard.displayBoard();
+        //check if win
+        //change active player turn
+        
     };
     return { 
         players, 
         playRound,
-        getActivePlayer 
+        getActivePlayer,
+        activePlayer
     };
 })();
+
+gameBoard.displayBoard();
